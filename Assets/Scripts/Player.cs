86de5caea73ead20273827a9e5a472a6f06cbccb
TxB0ne <xbungisxt.bone@gmail.com>
2026-02-24 18:07:07 -0500

    player and camera works

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed;
+    private Rigidbody rigidBody;
+    private Animator animator;
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+
+    }
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+
+        Move();
+
+    }
+
+    void Move()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+        Vector3 dir = new Vector3(xInput, 0, zInput) * moveSpeed;
+        dir.y = rigidBody.linearVelocity.y;
+        rigidBody.linearVelocity = dir;
+
+        Vector3 facingDir = new Vector3(xInput, 0, zInput);
+        if (facingDir.magnitude > 0)
+        {
+            transform.forward = facingDir;
+        }
+
+    }
+}
