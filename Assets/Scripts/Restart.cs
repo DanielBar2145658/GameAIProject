3921bbb424027497aecfc7bcbaa5298e69e2de02
TxB0ne <xbungisxt.bone@gmail.com>
2026-03-27 15:28:13 -0400

    restart button + restart when fall in water added

---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        RestartGame();
+    }
+}
